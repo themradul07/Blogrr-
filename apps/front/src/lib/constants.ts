61644b49df356e8c0319121084a952bf29d46501
MradulGandhi <gandhisir80@gmail.com>
2025-10-22 10:25:02 +0530

    changed backend url

--- apps/front/src/lib/constants.ts
+++ apps/front/src/lib/constants.ts
@@ -1,3 +1,3 @@
 // export const BACKEND_URL ="http://localhost:8000"
-export const BACKEND_URL = "https://blogrr111.onrender.com/"
+export const BACKEND_URL = "https://blogrr111.onrender.com"
 export const DEFAULT_PAGE_SIZE = 12
