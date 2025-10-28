6da2827ccc826a7ee5bbd52fbc7b891ec438973e
MradulGandhi <gandhisir80@gmail.com>
2025-10-28 08:41:57 +0530

    Port fixation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 8000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Server running on port ${port}`);
 }
 bootstrap();
